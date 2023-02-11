e1904ca4decca8b38e58dc5c26bd68536a4cd64f
Mark Anthony Llego <111579522+llegomark@users.noreply.github.com>
2023-02-12 00:34:36 +0800

    add: font weight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import localFont from '@next/font/local'
 
-const inter = localFont({ src: '../styles/Inter-Light.ttf' })
+const inter = localFont({ 
+  src: '../styles/Inter-Light.ttf',
+  weight: "300",
+  style: "normal",
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
