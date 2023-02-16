a920765de0cf7b9b855d18a18dfada33d0c6debb
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-02-17 03:38:03 +0800

    updated resizable panel

--- components/ResizablePanel.tsx
+++ components/ResizablePanel.tsx
@@ -1,18 +1,21 @@
-import { motion } from "framer-motion";
+import { motion, Transition } from "framer-motion";
 import useMeasure from "react-use-measure";
 
 interface ResizablePanelProps {
-  children: React.ReactNode;
+  children: React.ReactElement<HTMLDivElement>;
 }
 
 const ResizablePanel: React.FC<ResizablePanelProps> = ({ children }) => {
   const [ref, { height }] = useMeasure();
 
+  const animate = { height };
+  const transition: Transition = { type: "tween", duration: 0.5 };
+
   return (
     <motion.div
-      animate={{ height }}
+      animate={animate}
       className="relative w-full overflow-hidden"
-      transition={{ type: "tween", duration: 0.5 }}
+      transition={transition}
     >
       <div ref={ref} className="absolute inset-x-0">
         {children}
