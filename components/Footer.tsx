49f1a7a39b58184d1363310c8e3c2c01b12d5d2f
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-02-17 05:37:34 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import SocialIcon from "./SocialIcon";
 
 const currentYear = (): number => {
@@ -6,28 +5,41 @@ const currentYear = (): number => {
   return date.getFullYear();
 };
 
+function Copyright() {
+  return (
+    <a
+      href="https://llego.dev/"
+      target="_blank"
+      className="font-bold underline-offset-2 transition hover:underline"
+      aria-label="Visit Mark Anthony Llego's website"
+    >
+      Mark Anthony Llego
+    </a>
+  );
+}
+
+function TwitterLink() {
+  return (
+    <a
+      href="/twitter"
+      target="_blank"
+      rel="preconnect noopener noreferrer"
+      className="group"
+      aria-label="Visit Mark Anthony Llego's Twitter profile"
+    >
+      <SocialIcon platform="twitter" size={25} />
+    </a>
+  );
+}
+
 export default function Footer() {
   return (
     <footer className="mt-5 mb-3 flex h-16 w-full flex-col items-center justify-between space-y-2 border-t px-3 pt-4 text-center sm:mb-0 sm:h-20 sm:flex-row sm:pt-2">
       <div>
-        Copyright © {currentYear()}{" "}
-        <Link
-          href="https://llego.dev/"
-          className="font-bold underline-offset-2 transition hover:underline"
-        >
-          Mark Anthony Llego
-        </Link>
+        Copyright © {currentYear()} <Copyright />
       </div>
       <div className="flex space-x-3 sm:space-x-4">
-        <Link
-          href="/twitter"
-          target={"_blank"}
-          rel="noopener noreferrer"
-          className="group"
-          aria-label="Mark Anthony Llego on Twitter"
-        >
-          <SocialIcon platform="twitter" size={25} />
-        </Link>
+        <TwitterLink />
       </div>
     </footer>
   );
