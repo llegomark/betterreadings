71966f2d45c245922b10cd8a0aa28a4b02c87ed0
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-02-20 03:37:36 +0800

    optimized prompts

--- middleware.ts
+++ middleware.ts
@@ -15,10 +15,10 @@ interface RatelimitInfo {
 // Create a new ratelimit instance with Upstash Redis and sliding window strategy
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(6, "24 h"),
+  limiter: Ratelimit.slidingWindow(50, "24 h"),
   timeout: 2000, // 2 second
   analytics: true,
-  // ephemeralCache: new Map(),
+  ephemeralCache: new Map(),
 });
 
 /**
