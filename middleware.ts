a2462e0184999b5a3894440a8fb90fb0807712a4
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-02-19 10:50:58 +0800

    Add comments to rate limiting middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
-import type { NextFetchEvent, NextRequest} from "next/server";
+import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
+// Define an interface for the ratelimit information
 interface RatelimitInfo {
   success: boolean;
   pending: Promise<unknown>;
@@ -11,6 +12,7 @@ interface RatelimitInfo {
   remaining: number;
 }
 
+// Create a new ratelimit instance with Upstash Redis and sliding window strategy
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(5, "60 s"),
@@ -27,16 +29,21 @@ export default async function middleware(
   request: NextRequest,
   event: NextFetchEvent
 ): Promise<Response | undefined> {
+  // Get the user's IP address from the request, default to "127.0.0.1" if not present
   const ip = request.ip ?? "127.0.0.1";
 
   try {
+    // Use the ratelimit library to get information about the user's request limit
     const ratelimitInfo: RatelimitInfo = await ratelimit.limit(`mw_${ip}`);
-    event.waitUntil(ratelimitInfo.pending);
+    event.waitUntil(ratelimitInfo.pending); // Wait for the ratelimit to be enforced
 
+    // If the user has not exceeded the request limit, return a NextResponse.next() response
     const response = ratelimitInfo.success
       ? NextResponse.next()
-      : NextResponse.redirect(new URL("/api/blocked", request.url), request);
+      : // If the user has exceeded the request limit, redirect them to the "/api/blocked" endpoint
+        NextResponse.redirect(new URL("/api/blocked", request.url), request);
 
+    // Set headers in the response indicating the rate limit and remaining requests
     response.headers.set("X-RateLimit-Limit", ratelimitInfo.limit.toString());
     response.headers.set(
       "X-RateLimit-Remaining",
@@ -46,11 +53,13 @@ export default async function middleware(
 
     return response;
   } catch (err) {
+    // Log any errors and allow the request to proceed
     console.error("Error in rate limiting middleware:", err);
     return NextResponse.next();
   }
 }
 
+// Specify that this middleware should be applied to requests matching the "/api/generate" path
 export const config = {
   matcher: "/api/generate",
 };
--- pages/api/blocked.ts
+++ pages/api/blocked.ts
@@ -6,6 +6,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
  * @param response - The HTTP response object.
  */
 export default function handler(request: NextApiRequest, response: NextApiResponse) {
+  // Set the response status to 429 (Too Many Requests)
   response.status(429);
+  // Return a plain text message that indicates the user has exceeded the request limit and should try again later
   return response.end("Too many requests. Please try again later.");
 }
